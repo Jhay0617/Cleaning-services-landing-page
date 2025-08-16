1dd6f9a15b5f5b5d1cdff7d59fbf36b4b875d633
SaltyKid <jhay17mora@gmail.com>
2025-08-16 15:36:08 +0800

    finished the get started form

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 const btnQuote = document.getElementById('quote');
 const modal = document.querySelector('.modal-get-a_quote');
 const overlay = document.querySelector('.overlay');
-const closeBtn = document.querySelector('.btn-close');
+const closeBtn = document.querySelectorAll('.btn-close');
+const getStarted = document.querySelector('.get-started');
+const getBtn = document.querySelectorAll('.get-btn');
+
+getBtn.forEach(btn =>
+  btn.addEventListener('click', function (e) {
+    getStarted.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  })
+);
+
 btnQuote.addEventListener('click', function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 });
 
-closeBtn.addEventListener('click', function (e) {
-  e.preventDefault();
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+closeBtn.forEach(btn =>
+  btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+    getStarted.classList.add('hidden');
+  })
+);
